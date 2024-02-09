f86196f96172a6c9fe0dcf3c33a24085ab0e7a9a
Lych233 <zja_225799@126.com>
2024-02-09 19:30:21 +0800

    I forgot something in DispenserZombie

--- src/main/java/lych/polonium/entity/monster/DispenserZombie.java
+++ src/main/java/lych/polonium/entity/monster/DispenserZombie.java
@@ -32,46 +32,46 @@ public class DispenserZombie extends Zombie implements RangedAttackMob {
             super(type, level);
         }
 
-        public static AttributeSupplier.Builder createAttributes() {
-            return Zombie.createAttributes()
-                    .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, 0);
-        }
+    public static AttributeSupplier.Builder createAttributes() {
+        return Zombie.createAttributes()
+                .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, 0);
+    }
 
-        @Override
-        protected void randomizeReinforcementsChance() {
-            Objects.requireNonNull(getAttribute(Attributes.SPAWN_REINFORCEMENTS_CHANCE)).setBaseValue(0);
-        }
+    @Override
+    protected void randomizeReinforcementsChance() {
+        Objects.requireNonNull(getAttribute(Attributes.SPAWN_REINFORCEMENTS_CHANCE)).setBaseValue(0);
+    }
 
-        @Override
-        protected void registerGoals() {
-            goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8));
-            goalSelector.addGoal(8, new RandomLookAroundGoal(this));
-            addBehaviourGoals();
-        }
+    @Override
+    protected void registerGoals() {
+        goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8));
+        goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        addBehaviourGoals();
+    }
 
-        @Override
-        protected void addBehaviourGoals() {
-            goalSelector.addGoal(2, new RangedAttackGoal(this, 0.85, 40, 10));
-            goalSelector.addGoal(6, new MoveThroughVillageGoal(this, 1, true, 4, this::canBreakDoors));
-            goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1));
-            targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(ZombifiedPiglin.class));
-            targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-            targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
-            targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
-        }
+    @Override
+    protected void addBehaviourGoals() {
+        goalSelector.addGoal(2, new RangedAttackGoal(this, 0.85, 40, 10));
+        goalSelector.addGoal(6, new MoveThroughVillageGoal(this, 1, true, 4, this::canBreakDoors));
+        goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1));
+        targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(ZombifiedPiglin.class));
+        targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
+        targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
+    }
 
-        @Override
-        public void performRangedAttack(LivingEntity target, float power) {
-            Projectile projectile = findProjectileFor(target);
-            double targetY = target.getEyeY() - (double) 1.1F;
-            double dx = target.getX() - this.getX();
-            double dy = targetY - projectile.getY();
-            double dz = target.getZ() - this.getZ();
-            double yModifier = Math.sqrt(dx * dx + dz * dz) * (double) 0.2F;
-            projectile.shoot(dx, yModifier + dy, dz, 1.6F, 12.0F);
-            playSound(SoundEvents.DISPENSER_DISPENSE, 1, 0.4F / (getRandom().nextFloat() * 0.4F + 0.8F));
-            level().addFreshEntity(projectile);
-        }
+    @Override
+    public void performRangedAttack(LivingEntity target, float power) {
+        Projectile projectile = findProjectileFor(target);
+        double targetY = target.getEyeY() - (double) 1.1F;
+        double dx = target.getX() - this.getX();
+        double dy = targetY - projectile.getY();
+        double dz = target.getZ() - this.getZ();
+        double yModifier = Math.sqrt(dx * dx + dz * dz) * (double) 0.2F;
+        projectile.shoot(dx, yModifier + dy, dz, 1.6F, 12.0F);
+        playSound(SoundEvents.DISPENSER_DISPENSE, 1, 0.4F / (getRandom().nextFloat() * 0.4F + 0.8F));
+        level().addFreshEntity(projectile);
+    }
 
     @Override
     public void tick() {
